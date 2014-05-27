0ba0f69bc2ce9b100a8219caffb97db185a1b286
Joey Lawrance <lawrancej@wit.edu>
2014-05-27 12:45:50 -0400

    Dang, I should rethink this.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+
+
 class Main {
-	public static void main(String[] args) {
-		System.out.println("hi");
+	public static void main(String[] args) throws CoreException {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject("folly");
+		project.create(null);
+		project.open(null);
 	}
 }
